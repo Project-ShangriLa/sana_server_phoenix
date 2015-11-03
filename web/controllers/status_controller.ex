007cffbbf3839264b1bd9cc6292363516eddc5c9
AKB428 <otoraru@gmail.com>
2015-11-03 23:14:31 +0900

    fix controller

--- web/controllers/status_controller.ex
+++ web/controllers/status_controller.ex
@@ -2,16 +2,19 @@ defmodule SanaServerPhoenix.StatusController do
   use SanaServerPhoenix.Web, :controller
 
   def index(conn, _params) do
-    #パラメーターを受け取る _params["accounts"]
-    #アカウントリストを分解
-    #MySQLからデータを取得
-    #Baseテーブルを検索しIDを取得
-    #IDからTwitterのステートを取得
 
-  #  {:ok, result} = Mariaex.Connection.query(p, "SELECT * FROM bases where twitter_account IN '#{_params["accounts"]}'")
-  #  IO.inspect result
-    {:ok, result } = Ecto.Adapters.SQL.query(Repo, "SELECT * from bases", [])
-    IO.inspect result
+    #IDからTwitterのステートを取得
+    #  {:ok, result} = Mariaex.Connection.query(p, "SELECT * FROM bases where twitter_account IN '#{_params["accounts"]}'")
+    #  IO.inspect result
+    # http://hexdocs.pm/ecto/Ecto.Adapters.SQL.html#query/4
+    #パラメーターを受け取る _params["accounts"]
+
+    account_list = Enum.join(String.split(_params["accounts"],","), "\",\"")
+    account_list_st = "\"" <> account_list <> "\""
+
+    #Baseテーブルを検索しIDを取得
+    {:ok, result } = Ecto.Adapters.SQL.query(Repo, "SELECT bases.id, bases.twitter_account from bases where twitter_account IN (#{account_list_st})", [])
+    IO.inspect result[:rows]
 
     render conn, msg: "hogehoge" <> _params["accounts"]
   end
